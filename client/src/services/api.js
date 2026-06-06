1c2f19a97e6614b6a0a0890777cef9f5834ffc38
abhishekrana <ar7458909@gmail.com>
2026-06-07 03:16:54 +0530

    connected to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://miniexpensetrackerr-1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
