f52c257f51922242b6010aa28437992e5c1d698a
Abhishek_Rana <156297525+12abhishekrana@users.noreply.github.com>
2026-06-08 22:47:08 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://miniexpensetracker-3.onrender.com"
+  baseURL: "https://miniexpensetracker-3.onrender.com/api",
 });
 
 // attach token automatically
