23031fceb8e07985f5ddad95c5f9c5413406a5d8
Abhishek_Rana <156297525+12abhishekrana@users.noreply.github.com>
2026-06-09 02:00:27 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,12 +4,17 @@ const api = axios.create({
   baseURL: "https://miniexpensetracker-3.onrender.com/api",
 });
 
-// attach token automatically
+// attach headers automatically
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token"); // optional
+  const userId = localStorage.getItem("userId"); // IMPORTANT
 
   if (token) {
     config.headers.Authorization = token;
+  }
+
+  if (userId) {
+    config.headers.userid = userId;
   }
 
   return config;
