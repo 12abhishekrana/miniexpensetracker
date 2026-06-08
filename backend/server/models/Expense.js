5a0c88866da57d940179d943909c6c5241666458
Abhishek_Rana <156297525+12abhishekrana@users.noreply.github.com>
2026-06-09 02:03:45 +0530

    Remove userId from Expense model
    Removed userId field from Expense schema.

--- backend/server/models/Expense.js
+++ backend/server/models/Expense.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 const ExpenseSchema = new mongoose.Schema(
   {
-    userId: {
-      type: String,
-      required: true,
-    },
     amount: {
       type: Number,
       required: true,
